a984eccb8517d0c7ed1eb984480da7b41fba609d
david sureway <nwachukwudavid100@gmail.com>
2023-09-13 17:37:15 +0100

    .

---
+++ 1. basics/assignment.py
@@ -0,0 +1,16 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+# string manipulation
+print("hello" + " " + "world")
+
+# python input function and variables
+face = input("what is your name  \n")
+print(face)
+
+# todays project called band nickname generator
+band_name = input("what is name of your band  \n")
+pet_name = input("what is name of your favorite pet  \n")
+print("your band name could be " + band_name + " " + pet_name)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+9/12/2023 => Day 1 (5)
+9/13/2023 =>
+
 # 100daysOfCode
+
 projects related to majorly python language
+link to cloud code => https://replit.com/@appbrewery/day-1-printing-start
