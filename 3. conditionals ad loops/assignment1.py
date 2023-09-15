b7da6fd6f43f2645ae14158ec3e844a854262d86
david sureway <nwachukwudavid100@gmail.com>
2023-09-15 16:56:11 +0100

    .

---
+++ 3. conditionals ad loops/assignment1.py
@@ -0,0 +1,23 @@
+# if, elif and else statement
+# myNum = int(input("what is your number "))
+# if (myNum % 2 == 0):
+#     print("yes it is")
+# else:
+#     print("not at all")
+
+# practising if elif and else for ages pf kids
+# myAge = int(input("what is your number "))
+
+# if myAge < 5:
+#     print("10")
+# elif myAge < 10:
+#     print("20")
+# else:
+#     print("100")
+
+# logical operators or, and , not
+task = 30
+if task == 40 or task == 50:
+    print("yes")
+else:
+    print("no")
