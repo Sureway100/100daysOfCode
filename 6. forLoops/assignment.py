10e4154ae0889c2ade71cad7aa1bc3a98f2f800b
david sureway <nwachukwudavid100@gmail.com>
2023-09-22 23:40:54 +0100

    .

---
+++ 6. forLoops/assignment.py
@@ -0,0 +1,31 @@
+# fruits = ["apple", "cucumber", "oranges"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# # ages = [3, 4, 5, 6, 12, 45, 6, 77, 12, 12]
+# ages = input("list of ages ").split()
+# # print(ages)
+
+# totalAge = 0
+
+# for age in ages:
+#     totalAge += int(age)
+#     print(totalAge)
+
+# print("total ages ", totalAge)
+# print("ave ages ", totalAge / len(ages))
+
+
+# # highest score in an array
+# items = [23, 45, 12, 99, 41]
+
+# highest_num = 0
+# for item in items:
+#     if item > highest_num:
+#         highest_num = item
+# print("highest score is ", highest_num)
+
+
+# for loops with the range
+# for num in range()
--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 9/12/2023 => Day 1 (5)
 9/13/2023 => Day 1 (17)
 9/14/2023 => Day 2 (27)
-9/15/2023 => Day 3
+9/15/2023 => Day 3 ....missed
+/
+/
+/
+9/22/2023 => Day 5
 
 # 100daysOfCode
 
 projects related to majorly python language
 link to cloud code => https://replit.com/@appbrewery/day-1-printing-start
+
+https://haveibeenpwned.com/ => have been pawned
